034d5434767e3b5f537cfa5b5707082f94cf5b9e
Dane Ahern <danielaherniv@gmail.com>
2024-06-08 15:42:49 -0400

    Tring to Fix Select

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,5 +1,6 @@
 import React, {useState, useContext} from 'react'
 import { GlobalContext } from '../context/GlobalState';
+import Select from 'react-select';
 
 export const AddTransaction = () => {
   const [text, setText] = useState('');
@@ -8,42 +9,69 @@ export const AddTransaction = () => {
   const [amount, setAmount] = useState(0);
   const [phase, setPhase] = useState(0);
 
-  const { addTransaction } = useContext(GlobalContext);
+  function materialOptions () {
 
-  const onSubmit = e => {
-    e.preventDefault();
 
-    const newTransaction = {
-      id: Math.floor(Math.random() * 100000000),
-      text,
-      to,
-      from,
-      phase,
-      amount: +amount
-    }
+    const material = [
+      {label: "BRG"},
+      {label: "RCL"},
+      {label: "CBR"},
+    
+        ]
 
-    addTransaction(newTransaction);
+
   }
 
-  return (
-    <>
+  function NumberOptions () {
+
+
+    const number = [
+      {label: 1},
+      {label: 2},
+      {label: 1},
+    ]
+
+  }
+      
+  const { addTransaction } = useContext(GlobalContext);
+
+const onSubmit = e => {
+  e.preventDefault();
+
+const newTransaction = {
+  id: Math.floor(Math.random() * 100000000),
+text,
+to,
+from,
+phase,
+amount: +amount
+}
+
+addTransaction(newTransaction);
+}
+
+return (
+  <>
       <h3>Add Delivery</h3>
       <form onSubmit={onSubmit}>
       <div className="form-control">
           <label htmlFor="amount"
-            >QUAN <br />
-            (Positive - DELIVERED, Negative - PICKED UP)</label
-          >
-          <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+            >QUANTITY <br />
+            </label>
+          <input number={NumberOptions} type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
         </div>
+
 
         <div className="form-control">
           <label htmlFor="text">DESCRIPTION</label>
+      
           <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Product Delivered..." />
+
+
 
           <label htmlFor="text">TO</label>
           <input type="text" value={from} onChange={(e) => setFrom(e.target.value)} placeholder="Job / Pit #..." />
-          
+
           <label htmlFor="text">FROM</label>
           <input type="text" value={to} onChange={(e) => setTo(e.target.value)} placeholder="Job / Pit #..." />
 
@@ -53,13 +81,13 @@ export const AddTransaction = () => {
 
 {/* 
         <div className="form-control">
-          <label htmlFor="text">FROM</label>
-          <input type="text" value={to} onChange={(e) => setTo(e.target.value)} placeholder="Job / Pit #..." />
+        <label htmlFor="text">FROM</label>
+        <input type="text" value={to} onChange={(e) => setTo(e.target.value)} placeholder="Job / Pit #..." />
         </div> */}
         {/* <div className="form-control">
           <label htmlFor="text">TO</label>
           <input type="text" value={from} onChange={(e) => setFrom(e.target.value)} placeholder="Job / Pit #..." />
-        </div> */}
+          </div> */}
 
 
 
@@ -68,4 +96,5 @@ export const AddTransaction = () => {
       </form>
     </>
   )
+
 }
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -9,9 +9,14 @@ export const TransactionList = () => {
   return (
     <>
       <h3>History</h3>
+
       <ul className="list">
         {transactions.map(transaction => (<Transaction key={transaction.id} transaction={transaction} />))}
+
       </ul>
+
+
+
     </>
   )
 }
